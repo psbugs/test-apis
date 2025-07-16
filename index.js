454a86e29ad79546a8bafc44ab03f8e00b97a83f
Praveen Sunhare <praveen786sunhare@gmail.com>
2025-07-16 22:31:48 +0530

    correct the send and status for fetched producst

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const products = require('./src/utils/products.json')
-
+console.log('products',products)
 app.get("/hy-testing",(req,res)=>{
     res.status(200).send('Hy test response');
 })
@@ -12,7 +12,7 @@ app.get("/next-hy-testing",(req,res)=>{
 
 
 app.get('/fetch-products',(req,res)=>{
-    res.send(200).send(products);
+    res.status(200).send(products);
 })
 app.listen(3000,()=>{
     console.log('server is running')
