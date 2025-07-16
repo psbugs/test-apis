cedd5a3392cab58fe2be8b6418db6317e142267a
Praveen Sunhare <praveen786sunhare@gmail.com>
2025-07-16 22:28:30 +0530

    Added fetched products api.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const products = require('./src/utils/products.json')
 
 app.get("/hy-testing",(req,res)=>{
     res.status(200).send('Hy test response');
@@ -10,6 +10,10 @@ app.get("/next-hy-testing",(req,res)=>{
     res.status(200).send('Hy next test response');
 });
 
+
+app.get('/fetch-products',(req,res)=>{
+    res.send(200).send(products);
+})
 app.listen(3000,()=>{
     console.log('server is running')
 })
